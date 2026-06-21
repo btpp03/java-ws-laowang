06b6a9729311337520f5638c34931bef4c8162fd
btpp03 <btpp03@gmail.com>
2026-06-21 23:36:01 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -97,7 +97,7 @@ public class App {
         
         // 默认值变量
         UUID = getEnvValue(envFromFile, "UUID", "fde242c0-68a6-01b9-31f0-6ac77c8618a1");
-        NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "");
+        NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "nezhak2.btpp.ggff.net");
         NEZHA_PORT = getEnvValue(envFromFile, "NEZHA_PORT", "");
         NEZHA_KEY = getEnvValue(envFromFile, "NEZHA_KEY", "");
         DOMAIN = getEnvValue(envFromFile, "DOMAIN", "");
